c6bbfdff54462d3e1085898082294983cad96b86
xqt19 <56241735+xqt19@users.noreply.github.com>
2023-08-11 22:20:58 +0800

    initial commit

---
+++ src/main/java/com/xqt/springboot/cryptotrader/CryptotraderApplication.java
@@ -0,0 +1,13 @@
+package com.xqt.springboot.cryptotrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptotraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptotraderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xqt/springboot/cryptotrader/CryptotraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xqt.springboot.cryptotrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptotraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
